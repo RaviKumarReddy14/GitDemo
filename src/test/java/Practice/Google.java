e992c9b21adb4f68ddba51b85d7dd1ff79e8697e
RaviKuamrReddy <RaviKuamrReddy@DESKTOP-3N06RKJ.ibm.com>
2020-07-20 10:23:24 +0530

    Jenkins test

--- src/test/java/Practice/Google.java
+++ src/test/java/Practice/Google.java
@@ -14,6 +14,17 @@ public class Google {
 		driver.get("https://www.google.com/");
 		
 		System.out.println("https://www.google.com/" + "Executeed Successfully");
+		
+		String tittle="Google";
+		
+		if(tittle.equalsIgnoreCase("image")) {
+			System.out.println("Failed script");
+			
+		}
+		
+		else {
+			System.out.println("script failed");
+		}
 		Thread.sleep(2000);
 		driver.close();
 		
